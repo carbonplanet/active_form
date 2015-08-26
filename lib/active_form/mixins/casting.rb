7092ffe2430da6a201ef69375d97e48d46880652
Tom Tuddenham <ferrisoxide@gmail.com>
2015-08-26 14:27:42 +0930

    might fix things, might not

--- lib/active_form/mixins/casting.rb
+++ lib/active_form/mixins/casting.rb
@@ -1,12 +1,12 @@
 module ActiveForm
   module Mixins
     module Casting
-   
-      def self.included(base)    
+
+      def self.included(base)
         base.send(:extend, ClassMethods)
         base.send(:attr_reader, :type_cast)
       end
-   
+
       def cast_value(value)
         if self.respond_to?(:casting_filter)
           self.casting_filter(value)
@@ -16,8 +16,8 @@ module ActiveForm
           value
         end
       end
-    
-      def type_cast=(type = :string) 
+
+      def type_cast=(type = :string)
         if self.respond_to?("casting_for_#{type}")
           @type_cast = type.to_sym
           cast, intern = self.send("casting_for_#{type}")
@@ -25,45 +25,45 @@ module ActiveForm
           define_formatting_filter(intern) if intern
         end
       end
-    
+
       def casting_for_string
         [lambda { |value| value.is_a?(String) ? value : value.to_s }, nil]
       end
       alias :casting_for_text :casting_for_string
-    
+
       def casting_for_integer
         [lambda { |value| value.is_a?(Integer) ? value : value.to_i rescue value ? 1 : 0 }, nil]
       end
-    
+
       def casting_for_yaml
         [self.class.method_to_proc(:yaml_to_data), self.class.method_to_proc(:data_to_yaml)]
       end
-    
+
       def casting_for_float
         [lambda { |value| value.is_a?(Float) ? value : value.to_f rescue 0 }, nil]
       end
-    
+
       def casting_for_array
         [lambda { |value| value.is_a?(Array) ? value : [*value] }, lambda { |value| [*value].join(', ') }]
       end
-      
+
       def casting_for_datetime
         [self.class.method_to_proc(:string_to_time), self.class.method_to_proc(:time_to_string)]
       end
       alias :casting_for_timestamp :casting_for_datetime
-    
+
       def casting_for_time
         [self.class.method_to_proc(:string_to_dummy_time), self.class.method_to_proc(:time_to_string)]
       end
-    
+
       def casting_for_date
         [self.class.method_to_proc(:string_to_date), self.class.method_to_proc(:date_to_string)]
       end
-    
+
       def casting_for_boolean
         [self.class.method_to_proc(:value_to_boolean), self.class.method_to_proc(:boolean_to_value)]
       end
-    
+
       def define_casting_filter(prc = nil, &block)
         define_singleton_method(:casting_filter, &(block_given? ? block : prc))
       end
@@ -72,9 +72,9 @@ module ActiveForm
       def reset_casting_filter
         define_singleton_method(:casting_filter) rescue nil
       end
-   
+
       module ClassMethods
-      
+
         def define_casting_filter(prc = nil, &block)
           define_singleton_method(:casting_filter, &(block_given? ? block : prc))
         end
@@ -82,26 +82,26 @@ module ActiveForm
 
         def reset_casting_filter
           define_singleton_method(:casting_filter) rescue nil
-        end  
-        
+        end
+
         # type casting methods
-      
+
         def binary_to_string(value)
           value
         end
-        
+
         def data_to_yaml(value)
           value.to_yaml
         end
-        
+
         def yaml_to_data(value)
           return value unless value.is_a?(String)
           YAML::load(value) rescue nil
         end
-      
+
         def string_to_date(string)
           return string unless string.is_a?(String)
-          date_array = ParseDate.parsedate(string)
+          date_array = Date.parse(string)
           # treat 0000-00-00 as nil
           Date.new(date_array[0], date_array[1], date_array[2]) rescue nil
         end
@@ -117,7 +117,7 @@ module ActiveForm
             string = "#{current_time.year}-#{current_time.month}-#{current_time.day} #{string}"
           end
           # treat 0000-00-00 00:00:00 as nil
-          time_array = ParseDate.parsedate(string)[0..5]
+          time_array = Time.parse(string)[0..5]
           Time.send(ActiveForm::Element::Base.default_timezone, *time_array) rescue nil
         end
 
@@ -127,26 +127,26 @@ module ActiveForm
 
         def string_to_dummy_time(string)
           return string unless string.is_a?(String)
-          time_array = ParseDate.parsedate(string)
+          time_array = Time.parse(string)
           # pad the resulting array with dummy date information
           time_array[0] = 2000; time_array[1] = 1; time_array[2] = 1;
           Time.send(ActiveForm::Element::Base.default_timezone, *time_array) rescue nil
         end
-        
+
         def value_to_boolean(value)
           return value if value==true || value==false
           case value.to_s.downcase
           when "true", "t", "1" then true
           else false
           end
-        end      
-      
+        end
+
         def boolean_to_value(boolean)
           boolean ? "true" : "false"
         end
-      
+
       end
-  
+
     end
   end
 end
