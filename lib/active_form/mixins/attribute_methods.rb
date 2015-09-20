a167c7cab7c2ce3c20ad1857fe4e4e0b5cd57568
Ali Graham <ali.graham@bulletproof.net>
2015-09-20 16:38:52 +0930

    ruby 1.9 fix, use method_defined? rather than instance_methods with string

--- lib/active_form/mixins/attribute_methods.rb
+++ lib/active_form/mixins/attribute_methods.rb
@@ -1,33 +1,33 @@
 module ActiveForm::Mixins::AttributeMethods
-  
+
   def self.included(base)
     [:type].each { |m| base.send(:undef_method, m) rescue nil }
     base.send(:extend, ClassMethods)
   end
-  
+
   def attributes
     @attributes ||= HashWithIndifferentAccess.new
   end
-  
+
   def default_attributes
     { :id => identifier, :class => css }
   end
-  
+
   def attribute_names
     [:style, :javascript, :title, :lang] + self.class.element_attribute_names.to_a
   end
-  
+
   module ClassMethods
-    
+
     def define_attributes(*attrs)
       attrs.push(:lang) unless self.element_attribute_names.include?(:lang)
-      attrs.flatten.each do |attribute|      
-        self.element_attribute_names += [attribute.to_sym]  
-        define_method("#{attribute}=")  { |value| attributes[attribute] = value } unless instance_methods.include?("#{attribute}=")        
-        define_method("#{attribute}")   { attributes[attribute] } unless instance_methods.include?("#{attribute}")
+      attrs.flatten.each do |attribute|
+        self.element_attribute_names += [attribute.to_sym]
+        define_method("#{attribute}=")  { |value| attributes[attribute] = value } unless method_defined?("#{attribute}=")
+        define_method("#{attribute}")   { attributes[attribute] } unless method_defined?(attribute)
       end
     end
-    
+
   end
-  
+
 end
